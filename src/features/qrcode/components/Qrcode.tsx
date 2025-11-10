a31d9a22389100f61e90d0ed5ff472c6a685b2ce
lucas <lucaselorenzohenrique@gmail.com>
2025-11-10 13:53:18 -0300

    page qrcode

--- src/features/qrcode/components/Qrcode.tsx
+++ src/features/qrcode/components/Qrcode.tsx
@@ -1,17 +1,29 @@
+import './index.css';
 import { useEffect } from "react";
 import { useQrcode } from "../context/QrcodeContext";
 
 export const Qrcode = () => {
+  const { qrcode, getQrcode } = useQrcode();
 
-    const {qrcode, setQrcode, getQrcode } = useQrcode();
+  useEffect(() => {
+    if (!qrcode) getQrcode();
+  }, [qrcode, getQrcode]);
 
-    useEffect(() => {
-        if(!qrcode) getQrcode();
-    }, []);
-
-    return (
-        <div className="container-fluid d-flex justify-content-center align-items-center bg-dark">
-            <h1 className="text-white">Tailwind funcionando 🎉</h1>
-        </div>
-    );
-}
+  return (
+    <div className="qrcode-page">
+      <div className="qrcode-card">
+        <img src="/imagens_logo/LogoSegmix_login.png" alt="Segmix Logo" className="qrcode-logo" />
+        <h1 className="qrcode-title">Acesse via telegram</h1>
+        {qrcode ? (
+          <img src={qrcode} alt="QR Code Pix" className="qrcode-img" />
+        ) : (
+          <div className="qrcode-loading">
+            <div className="spinner"></div>
+            <p>Gerando QR Code...</p>
+          </div>
+        )}
+        <p className="qrcode-footer">Segmix Soluções Tecnológicas © 2025</p>
+      </div>
+    </div>
+  );
+};
